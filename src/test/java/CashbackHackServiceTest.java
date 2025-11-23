de6565693e10adeb02293fc7b80bb8f0bcc91218
Alexandr <0231050@mail.ru>
2025-11-23 21:10:00 +0300

    testng

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,62 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturn100WhenAmount900() {
+        ru.netology.service.CashbackHackService service = new ru.netology.service.CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0WhenAmount1000() {
+        ru.netology.service.CashbackHackService service = new ru.netology.service.CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn900WhenAmount1100() {
+        ru.netology.service.CashbackHackService service = new ru.netology.service.CashbackHackService();
+        int amount = 1100;
+
+        int actual = service.remain(amount);
+        int expected = 900;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0WhenAmount2000() {
+        ru.netology.service.CashbackHackService service = new ru.netology.service.CashbackHackService();
+        int amount = 2000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999WhenAmount1() {
+        ru.netology.service.CashbackHackService service = new ru.netology.service.CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
